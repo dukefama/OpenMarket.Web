6f8cfadf8ff8cece577cb5656c8a7defcdd329ac
dukefama <tunde.famakinwa@gmail.com>
2020-11-22 22:17:18 +0100

    First Commit for Open Market web

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OpenMarket.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
